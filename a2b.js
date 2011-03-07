f4151b33e42824aa2589b902525cf9c4193428d8
Jan Kleinert <jankleinert@gmail.com>
2011-03-06 20:52:04 -0500

    simplifying code for a2b.js and updating the background file

--- a2b.js
+++ a2b.js
@@ -1,14 +1,9 @@
 chrome.management.onInstalled.addListener(function(info) {
   var id;
   if (info.isApp) {
-    chrome.bookmarks.getTree(function(results) {
-      if (results[0].children != undefined) {
-        var children = results[0].children;
-        chrome.bookmarks.create({'parentId': children[0].id,
-                                   'url': info.appLaunchUrl }, 
-                                    function(result) {
-        }); 
-      }
-    });
+    // Using an id of 1 is a "reasonably safe" way of getting to
+    // the Bookmarks Bar according to http://crbug.com/21330
+    chrome.bookmarks.create({'parentId': '1',
+                             'url': info.appLaunchUrl}); 
   } 
 });
